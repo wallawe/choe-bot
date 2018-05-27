537f25e49f497946f5371a5a6de0ee355b6a0edc
William <wallac.will@gmail.com>
2018-05-27 16:35:28 -0400

    Do everything right

--- app.js
+++ app.js
@@ -1,84 +1,94 @@
+require('dotenv').load()
+const express = require('express')
+const bodyParser = require('body-parser')
+const ENV = process.env.NODE_ENV || 'development'
 const { RTMClient, WebClient } = require('@slack/client')
 const ccxt = require('ccxt')
-
-const TOKEN = 'xoxp-310635658535-368680301281-370449133009-7f733849197596c5636d28047e2f73f4'
-const GENERAL_CHANNEL_ID = 'C93H12QMQ'
-const RODERICK_CHANNEL_ID = 'C94CQRQSJ'
-const TEST_CHANNEL_ID = 'C7YAV76C8'
-const TEST_TOKEN = 'xoxp-272105539559-272105539879-370332106784-96b5492427d26c35cdd07f55e5893358'
-
-const BINANCE_API = 'y25BplRP0DMxqeiiUcpd8DZXnTNKHujbtLw19RmtqyQIqyox3krSbH9Jo7ZPkLvf'
-const BINANCE_SECRET = 'HsNm2DaPFe3Wr0MQCogvvSupCc4moSvmxfRmGnRYN4ORSOdCHUMXBwBi4EvvjyPg'
 
 const VALID_SIGNAL = new RegExp(/Signal [\d]{2,}: [A-Z]{2,6}\/[A-Z]{2,6}/)
 const PAIR = new RegExp(/[A-Z]{3,5}\/[A-Z]{3,5}/)
 const RISK_LEVELS = ['high', 'medium', 'low']
 
+let app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const rtm = new RTMClient(TEST_TOKEN)
+app.get('/', (req, res) => { res.send('\n 👋 🌍 \n') })
 
-rtm.start()
+function getShitPoppin() {
 
-console.log('*** Realtime connection to Slack started ***')
+  const rtm = new RTMClient(process.env.TEST_TOKEN)
+  rtm.start()
 
-let binance = new ccxt.binance ({
-    apiKey: BINANCE_API,
-    secret: BINANCE_SECRET,
-});
+  console.log('*** Realtime connection to Slack started ***')
 
-rtm.on('message', async message => {
+  let binance = new ccxt.binance ({
+    apiKey: process.env.BINANCE_API,
+    secret: process.env.BINANCE_SECRET,
+  });
 
-  const {text} = message
+  rtm.on('message', async message => {
 
-  if (message.type === 'message' && message.channel === TEST_CHANNEL_ID ) {
-    console.log(text)
+    const {text} = message
 
-    // we can use riskLevel later to determine how much to buy. lower risk == higher amount
-    // it will be low, medium, or high
-    const riskLevel = RISK_LEVELS.find(level => text.toLowerCase().includes(level))
+    if (message.type === 'message' && message.channel === process.env.TEST_CHANNEL_ID ) {
+      console.log(text)
 
-    // make sure it's a roderick signal and not just Eric chiming in with thoughts/opinions
-    if (text && VALID_SIGNAL.test(text) && !!riskLevel) {
+      // we can use riskLevel later to determine how much to buy. lower risk == higher amount
+      // it will be low, medium, or high
+      const riskLevel = RISK_LEVELS.find(level => text.toLowerCase().includes(level))
 
-      // grab the pairing from the string
-      const pairing = text.match(PAIR)[0].trim()
+      // make sure it's a roderick signal and not just Eric chiming in with thoughts/opinions
+      if (text && VALID_SIGNAL.test(text) && !!riskLevel) {
 
-      // Coin we want to buy
-      const buy = pairing.split('/')[0]
-      // Coin we want to sell
-      const sell = pairing.split('/')[1]
+        // grab the pairing from the string
+        const pairing = text.match(PAIR)[0].trim()
 
-      // check for inclusion of a risk level even though we're not using it right now.
-      // this is so we don't trigger a purchase when he gives the results of a signal.
-      if (text.includes('LONG')) {
+        // Coin we want to buy
+        const buy = pairing.split('/')[0]
+        // Coin we want to sell
+        const sell = pairing.split('/')[1]
 
-        console.log('going long here')
+        // check for inclusion of a risk level even though we're not using it right now.
+        // this is so we don't trigger a purchase when he gives the results of a signal.
+        if (text.includes('LONG')) {
 
-        const RISK_AMOUNT = 0.25 // amount of bitcoin to risk on any given trade. we can get more advanced later
-        const SLIPPAGE_TOLERANCE = 0.003 // % above last ask we're willing to pay in case another bot beats us
+          console.log('going long here')
 
-        const tokenInfo = await binance.fetchTicker(pairing)
-        const askingPrice = tokenInfo.ask
-        const maxBuyPrice = askingPrice * (1 + SLIPPAGE_TOLERANCE)
-        const buyAmount = RISK_AMOUNT / parseFloat(askingPrice)
+          const RISK_AMOUNT = 0.25 // amount of bitcoin to risk on any given trade. we can get more advanced later
+          const SLIPPAGE_TOLERANCE = 0.004 // % above last ask we're willing to pay in case another bot beats us
 
-        console.log('buy amount: ', buyAmount)
-        console.log('max price: ', maxBuyPrice)
-        // const purchase = await binance.createLimitBuyOrder(pairing, buyAmount, maxBuyPrice)
-        console.log('result of attempted purchase: ', purchase)
+          const tokenInfo = await binance.fetchTicker(pairing)
+          const askingPrice = tokenInfo.ask
+          const maxBuyPrice = askingPrice * (1 + SLIPPAGE_TOLERANCE)
+          const buyAmount = RISK_AMOUNT / parseFloat(askingPrice)
 
-      } else if (text.includes('SHORT')) {
+          console.log('buy amount: ', buyAmount)
+          console.log('max price: ', maxBuyPrice)
+          // const purchase = await binance.createLimitBuyOrder(pairing, buyAmount, maxBuyPrice)
+          // console.log('result of attempted purchase: ', purchase)
 
-        console.log('going short here')
+        } else if (text.includes('SHORT')) {
 
+          console.log('going short here')
+
+        }
       }
     }
+  })
+}
+
+app.listen(process.env.PORT, (err) => {
+  if (err) throw err
+
+  if (process.env.SLACK_TOKEN) {
+    getShitPoppin()
   }
 })
 
 // not currently in use
 const listChannels = async function() {
-  const web = new WebClient(TOKEN)
+  const web = new WebClient(process.env.SLACK_TOKEN)
   const res = await web.channels.list()
   const channels = res.channels.map(ch => `${ch.name}: ${ch.id}`)
 
