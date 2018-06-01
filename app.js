1e0a4840357a0d59b2334395382b97c0c944593b
William <wallac.will@gmail.com>
2018-05-31 21:47:32 -0400

    turn off for vegas

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => { res.send('\n 👋 🌍 \n') })
 
 function getShitPoppin() {
-
+  return // off
   const rtm = new RTMClient(process.env.SLACK_TOKEN)
   rtm.start()
 
